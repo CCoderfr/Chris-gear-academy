f3ea93f1b42eb0be6dc09f546db9d6a135e754f7
CCoderfr <chrisadamsdrums@gmail.com>
2024-05-23 20:48:33 -0400

    Update lib.rs

--- pebbles-game/src/lib.rs
+++ pebbles-game/src/lib.rs
@@ -1,1 +1,21 @@
+#![no_std]
 
+use gstd::{};
+use pebbles_game_io::*;
+
+static nut pebbles_game: Option<GameState> = None;
+
+#[no_mangle]
+extern "C" fn init()<
+ // MY Code
+  )
+
+#[no_mangle]
+extern "C" fn handle()<
+ // MY Code
+  )
+
+#[no_mangle]
+extern "C" fn state()<
+ // MY Code
+  )
